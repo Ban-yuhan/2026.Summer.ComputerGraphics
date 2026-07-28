2a458dae13ca7620186e2347ac427c1ef83da5c8
Ban-yuhan <kimjinwoongs@gmail.com>
2026-07-28 15:11:09 +0900

    26.07.28 CircularLikedList

--- DataStruct/LinkedLink/LinkedLink/CircularLinkedList/CircularLinkedList/main.cpp
+++ DataStruct/LinkedLink/LinkedLink/CircularLinkedList/CircularLinkedList/main.cpp
@@ -11,6 +11,10 @@ struct SNode
 
 SNode* CreateNode(SNode* pStart, SNode* pNode, int data);
 void PrintLinkedList(SNode* pStart);
+SNode* FindeNodeData(SNode* pStart, int data);
+SNode* InsertNodeData(SNode* pStart, int data, int insert);
+void DeleteNodeData(SNode* pStart, int del);
+SNode* DeleteLinkedList(SNode* pStart);
 
 
 void main()
@@ -29,6 +33,19 @@ void main()
 
 	PrintLinkedList(pBegin);
 
+	SNode* pFind = FindeNodeData(pBegin, 50);
+	if (pFind != NULL)
+		printf("Find:%d\n", pFind->nData);
+
+	pEnd = InsertNodeData(pBegin, 20, 60);//노드 삽입
+
+	PrintLinkedList(pBegin);
+
+	DeleteNodeData(pBegin, 60);//노드 삭제
+
+	PrintLinkedList(pBegin);
+
+	pBegin = DeleteLinkedList(pBegin); //모든노드삭제 - 이 함수를 호출하지않을시 메모리가 누수됨.
 }
 
 
@@ -58,6 +75,82 @@ SNode* CreateNode(SNode* pStart, SNode* pNode, int data)
 }
 
 
+SNode* FindeNodeData(SNode* pStart, int data)
+{
+	SNode* pNode = pStart;
+	
+	while (pNode->nData != data)
+	{
+		pNode = pNode->pNext;
+	}
+
+	return pNode;
+}
+
+
+SNode* InsertNodeData(SNode* pStart, int data, int insert)
+{
+	SNode* pNode = NULL;
+	SNode* pInsert = NULL;
+
+	pNode = FindeNodeData(pStart, data);
+
+	pInsert = new SNode();
+	pInsert->nData = insert;
+	pInsert->pNext = pNode->pNext;
+	pNode->pNext = pInsert;
+
+	SNode* pEnd = pInsert;
+	while (pEnd->pNext != pStart)
+	{
+		pEnd = pEnd->pNext;
+	}
+
+	return pEnd;
+}
+
+
+void DeleteNodeData(SNode* pStart, int del)
+{
+	SNode* pNode = pStart;
+	SNode* pPre = NULL;
+
+	if (pNode->pNext != pStart) 
+	{
+		while (pNode->nData != del)
+		{
+			pPre = pNode;
+			pNode = pNode->pNext;
+		}
+	}
+
+	pPre->pNext = pNode->pNext;
+
+	delete pNode;
+}
+
+
+SNode* DeleteLinkedList(SNode* pStart)
+{
+	SNode* pNode = pStart;
+	SNode* pDel = NULL;
+
+	if (pStart != NULL) 
+	{
+		while (pNode->pNext != pStart) 
+		{
+			pDel = pNode;
+			pNode = pNode->pNext;
+			delete pDel;
+		}
+
+		delete pNode;
+	}
+	
+	return NULL;
+}
+
+
 void PrintLinkedList(SNode* pStart) 
 {
 	SNode* pNode = pStart;
@@ -67,14 +160,10 @@ void PrintLinkedList(SNode* pStart)
 		printf("%d", pNode->nData);
 		pNode = pNode->pNext;	
 
-		if (pNode != pStart) 
+		printf(",");
+		
+		if (pNode == pStart)
 		{
-			printf(",");
-		}
-		else 
-		{
-			printf(",");
-
 			printf("%d", pNode->nData);
 			break;
 		}
--- DataStruct/LinkedLink/LinkedLink/DoublyLinkedList/DoublyLinkedList/main.cpp
+++ DataStruct/LinkedLink/LinkedLink/DoublyLinkedList/DoublyLinkedList/main.cpp
@@ -8,3 +8,60 @@ struct SNode
 	SNode* pNext;
 	SNode* pBefore;
 };
+
+
+SNode* CreateNode(SNode* pNode, int data);
+SNode* FindNodeData(SNode* pStart, int data);
+void PrintLinkedList(SNode* Start);
+
+
+void main()
+{
+	SNode* pBegin = NULL;
+	SNode* pEnd = NULL;
+
+	pEnd = CreateNode(pEnd, 10);
+
+	pBegin = pEnd;
+
+	pEnd = CreateNode(pEnd, 20);
+	pEnd = CreateNode(pEnd, 30);
+	pEnd = CreateNode(pEnd, 40);
+	pEnd = CreateNode(pEnd, 50);
+
+	SNode* pFind = FindNodeData(pBegin, 40);
+	printf("FInd : %d \n", pFind->nData);
+
+
+}
+
+
+SNode* CreateNode(SNode* pNode, int data)
+{
+	SNode* pTemp = NULL;
+
+	pTemp = new SNode();
+
+	if (pNode != NULL)
+	{
+		pNode->pNext = pTemp;
+		pTemp->pBefore = pNode;
+	}
+	pTemp->nData = data;
+
+	return pTemp;
+}
+
+
+SNode* FindNodeData(SNode* pStart, int data)
+{
+	SNode* pNode = pStart;
+
+	while (pNode->nData != data)
+	{
+		pNode = pNode->pNext;
+	}
+
+	return pNode;
+}
+
